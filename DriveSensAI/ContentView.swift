20482ba0055f6043c164cd142b403bdc009da8b3
Md Abdullah Al Mazid <mazid.rafee@gmail.com>
2026-09-17 16:02:13 -0400

    Initial Commit

---
+++ DriveSensAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DriveSensAI
+//
+//  Created by Md Abdullah Al Mazid on 9/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DriveSensAI/DriveSensAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveSensAIApp.swift
+//  DriveSensAI
+//
+//  Created by Md Abdullah Al Mazid on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DriveSensAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
